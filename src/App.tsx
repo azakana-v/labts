58445890442cbd33f3e556878d1049c43a66bc1b
azakana-v <victor.azakana@gmail.com>
2024-08-01 18:18:15 -0300

    finished phone preview

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,19 @@ import Topbar from "./components/Topbar";
 import CardOrg from "./components/CardOrg";
 import Klogoverde from "./assets/klogoverde.png";
 import Placeholder from "./assets/placeholder.png";
-import { TextField, useTheme } from "@mui/material";
+import {
+  Box,
+  MenuItem,
+  Modal,
+  Select,
+  TextField,
+  useTheme,
+} from "@mui/material";
 import { theme } from "./main";
 import { useEffect, useState } from "react";
 import PhonePreview from "./components/PhonePreview";
+
+import Orgs from "./orgs.json";
 
 const Container = styled.div`
   display: flex;
@@ -109,23 +118,136 @@ const PlusText = styled.span`
   }
 `;
 
+const ModalTitle = styled.p`
+  font-size: 20px;
+  color: green;
+  margin: 0 auto;
+  font-weight: 600;
+  font-family: sans-serif;
+  border-bottom: 2px solid green;
+  padding-bottom: 10px;
+`;
+const AddButton = styled.button`
+  padding: 15px;
+  background-color: green;
+  border: none;
+  cursor: pointer;
+  color: white;
+  border-radius: 10px;
+  transition: 0.2s;
+  font-family: sans-serif;
+  font-size: 20px;
+  &:hover {
+    scale: 1.05;
+  }
+`;
+const ModalBox = styled.div`
+  width: 20vw;
+  height: 20vw;
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+  padding: 50px;
+  &:focus {
+    outline: none;
+    position: absolute;
+  }
+`;
+
+type Variavel = {
+  id: number;
+  value: string;
+  name: string;
+};
+
 function App() {
   const [varCount, setVarCount] = useState(0);
-  const [varsArrays, setVarsArrays] = useState([{ id: 0, content: "" }]);
-
-  useEffect(() => {}, [varsArrays]);
+  const [varsArrays, setVarsArrays] = useState<Variavel[]>([]);
+  const [previa, setPrevia] = useState<string>("");
+  const [org, setOrg] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [apelido, setApelido] = useState<string>("");
+  const [namespace, setNamespace] = useState<string>("");
+  const [elementname, setElementname] = useState<string>("");
+  const [id, setId] = useState<string>("");
+  const [open, setOpen] = useState<boolean>(false);
 
   const addVar = (id: number) => {
-    if (varCount < 6) {
-      varsArrays.push({ id: id, content: "" });
-      setVarCount(varCount + 1);
-      console.log(varsArrays);
-    }
+    varsArrays?.push({ id: id, value: "", name: name.toLowerCase() });
+    setVarCount(varCount + 1);
+    console.log(varsArrays);
   };
 
+  useEffect(() => {
+    console.log(varsArrays);
+  }, [varsArrays]);
+
+  const handleChange =
+    (index: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newVarsArrays = varsArrays?.map((input: any, i: any) => {
+        if (i === index) {
+          return { ...input, value: e.target.value };
+        }
+        return input;
+      });
+      setVarsArrays(newVarsArrays);
+    };
+
   const themePallete: any = useTheme().palette;
+
+  function contarOcorrencias(templateString: string) {
+    const matches = templateString.match(/\$\{/g);
+    return matches ? matches.length : 0;
+  }
+
+  function substituirValores(str: any, vars: any) {
+    const valores = vars.reduce((acc: any, item: any) => {
+      acc[item.name] = item.value;
+      return acc;
+    }, {});
+
+    return str.replace(/\$\{([^}]+)\}/g, function (match: any, p1: any) {
+      return valores[p1] !== undefined ? valores[p1] : match;
+    });
+  }
+
   return (
     <Wrapper>
+      <Modal
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <ModalBox>
+          <ModalTitle>Adicionar váriavel?</ModalTitle>
+
+          <TextField
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            id="outlined-basic"
+            label="Nome"
+            variant="outlined"
+          />
+          <AddButton
+            onClick={() => {
+              addVar(varCount + 1);
+              setOpen(false);
+              setName("");
+            }}
+          >
+            Adicionar
+          </AddButton>
+        </ModalBox>
+      </Modal>
       <Topbar />
       <Container className="App">
         <MainContentContainer>
@@ -138,31 +260,56 @@ function App() {
                 size="small"
                 label="Namespace"
                 variant="outlined"
+                value={namespace}
+                onChange={(e) => {
+                  setNamespace(e.target.value);
+                }}
               />
               <TextField
                 id="outlined-basic"
                 size="small"
                 label="Id"
                 variant="outlined"
+                value={id}
+                onChange={(e) => {
+                  setId(e.target.value);
+                }}
               />
               <TextField
                 id="outlined-basic"
                 size="small"
                 label="Elementname"
                 variant="outlined"
+                value={elementname}
+                onChange={(e) => {
+                  setElementname(e.target.value);
+                }}
               />
+              <Select
+                style={{ color: "black" }}
+                defaultValue="0"
+                // id="outlined-basic"
+                size="small"
+                inputProps={{ "aria-label": "Without label" }}
+                // variant="outlined"
+                onChange={(e) => {
+                  setOrg(e.target.value);
+                }}
+              >
+                {Orgs.orgNames.map((item, index) => {
+                  return <MenuItem value={item}>{item}</MenuItem>;
+                })}
+              </Select>
               <TextField
-                select
-                defaultValue="EUR"
                 id="outlined-basic"
                 size="small"
-                label="Org"
-                variant="outlined"
-              />
-              <TextField
-                id="outlined-basic"
-                size="small"
-                label="Prévia"
+                label={`Prévia - Número de variaveis: ${contarOcorrencias(
+                  previa
+                )} / ${varsArrays.length}`}
+                value={previa}
+                onChange={(e) => {
+                  setPrevia(e.target.value);
+                }}
                 variant="outlined"
               />
               <TextField
@@ -170,6 +317,10 @@ function App() {
                 size="small"
                 label="Apelido (Opcional)"
                 variant="outlined"
+                onChange={(e) => {
+                  setApelido(e.target.value);
+                }}
+                value={apelido}
               />
             </FormContainer>
             <VarLabelContainer>
@@ -178,25 +329,38 @@ function App() {
               <Line />
             </VarLabelContainer>
             <FormContainer>
-              {varsArrays.map((input, index) => {
+              {varsArrays?.map((input, index) => {
                 return (
                   <TextField
+                    key={input.id}
+                    onChange={handleChange(index)}
+                    value={input.value}
                     className="input-animation"
                     id="outlined-basic"
                     size="small"
-                    label={`Variavel ${varsArrays[index].id + 1}`}
+                    label={`${varsArrays[index].name}`}
                     variant="outlined"
                   />
                 );
               })}
             </FormContainer>
             <PlusButtonContainer>
-              <PlusText onClick={() => addVar(varCount + 1)}>+</PlusText>
+              <PlusText
+                onClick={() => {
+                  setOpen(true);
+                }}
+              >
+                +
+              </PlusText>
             </PlusButtonContainer>
           </LeftContainer>
 
           <RightContainer>
-            <PhonePreview></PhonePreview>
+            <PhonePreview
+              vars={varsArrays}
+              org={org}
+              previa={substituirValores(previa, varsArrays)}
+            ></PhonePreview>
           </RightContainer>
         </MainContentContainer>
       </Container>
--- src/components/PhonePreview.tsx
+++ src/components/PhonePreview.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import PhoneImg from "../assets/phonemockup.png";
 import WhatsImg from "../assets/whatsimg.png";
 import styled from "styled-components";
+import PerfilPlaceHolder from "../assets/klogo.png";
+import OthersIMG from "../assets/others.png";
 
 const Container = styled.div`
   width: 100%;
@@ -15,7 +17,7 @@ const Container = styled.div`
 const PhoneContainer = styled.div`
   width: 210px;
   height: 400px;
-  scale: 1.2;
+  scale: 1.4;
   background-image: url(${PhoneImg});
   background-repeat: no-repeat;
   background-size: contain;
@@ -29,55 +31,173 @@ const ContentContainer = styled.div`
   background-position: center;
   width: 170px;
   height: 350px;
-  scale: 1.2;
+  scale: 1.4;
   position: absolute;
   z-index: 1;
 `;
 
-const NameBarContainer = styled.div``;
+const NameBarContainer = styled.div`
+  margin-bottom: 10px;
+`;
 const NameBarEmpty = styled.div`
   width: 100%;
-  height: 20px;
+  height: 16px;
+  margin-top: 6px;
   background-color: #043e36;
 `;
 const NameBar = styled.div`
   width: 100%;
   height: 30px;
-  background-color: #064b43;
+  background-color: #075e54;
   position: relative;
   top: -1px;
   display: flex;
+  font-family: sans-serif;
+  align-items: center;
   color: white;
 `;
 
-const PerfilPhoto = styled.div`
+const PerfilPhotoContainer = styled.div`
+  max-width: 30%;
   width: 30%;
-`;
-const OrgNameContainer = styled.div`
-  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 2px 0px;
 `;
 
-const OrgName = styled.div``;
-const Subtitle = styled.div``;
-const Others = styled.div`
+const PerfilPhoto = styled.img`
+  border-radius: 50%;
+  background-color: #ffffff29;
+  max-height: 24px;
+  max-width: 24px;
+  min-height: 24px;
+  min-width: 24px;
+`;
+
+const OrgNameContainer = styled.div`
   width: 40%;
 `;
 
-const PhonePreview = () => {
+const OrgName = styled.p`
+  font-size: 10px;
+`;
+const Subtitle = styled.p`
+  font-size: 6px;
+  color: #c7c7c7;
+  font-weight: 300;
+`;
+const Others = styled.div`
+  width: 30%;
+`;
+
+const OthersImg = styled.img`
+  width: 100%;
+  padding-right: 6px;
+`;
+
+const MessageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const RightMessageContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: right;
+  justify-content: right;
+  padding-right: 10px;
+`;
+
+const LeftMessageContainer = styled.div`
+  width: 100%;
+  align-items: left;
+  margin-bottom: 10px;
+  display: flex;
+`;
+
+const LeftMessage = styled.p`
+  max-width: 60%;
+  background-color: white;
+  font-size: 9px;
+  font-family: sans-serif;
+  padding: 5px;
+  border-radius: 5px;
+  overflow: hidden;
+  position: relative;
+  left: -3px;
+`;
+
+const RightMessage = styled.p`
+  max-width: 60%;
+  background-color: #aed588;
+  font-size: 9px;
+  font-family: sans-serif;
+  padding: 5px;
+  border-radius: 5px;
+  position: relative;
+  right: -3px;
+  overflow: hidden;
+`;
+
+const PhonePreview = (props: any) => {
+  function trataStringOrg(org: string) {
+    if (org.length > 15) {
+      return org.substring(0, 8) + "...";
+    }
+    return org;
+  }
+
   return (
     <Container>
       <ContentContainer>
         <NameBarContainer>
           <NameBarEmpty />
           <NameBar>
-            <PerfilPhoto>photo</PerfilPhoto>
+            <PerfilPhotoContainer>
+              <PerfilPhoto src={PerfilPlaceHolder} />
+            </PerfilPhotoContainer>
             <OrgNameContainer>
-              <OrgName>Brisanet</OrgName>
-              <Subtitle>teste</Subtitle>
+              <OrgName>
+                {props.org ? trataStringOrg(props.org) : "Nome da Org"}
+              </OrgName>
+              <Subtitle>Ultima vez visto 20:00</Subtitle>
             </OrgNameContainer>
-            <Others>coisas</Others>
+            <Others>
+              <OthersImg src={OthersIMG} />
+            </Others>
           </NameBar>
         </NameBarContainer>
+        <MessageContainer>
+          <LeftMessageContainer>
+            <svg
+              width="7"
+              height="8"
+              viewBox="0 0 7 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M0.5 0H7V8L0.5 0Z" fill="#F7F7F7" />
+            </svg>
+
+            <LeftMessage>
+              {props.previa ? props.previa : "Digite uma prévia!"}
+            </LeftMessage>
+          </LeftMessageContainer>
+          <RightMessageContainer>
+            <RightMessage> Obrigado, Mario você é o cara!</RightMessage>
+            <svg
+              width="6"
+              height="6"
+              viewBox="0 0 6 6"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M5.5 0H0V6L5.5 0Z" fill="#AED588" />
+            </svg>
+          </RightMessageContainer>
+        </MessageContainer>
       </ContentContainer>
       <PhoneContainer></PhoneContainer>
     </Container>
